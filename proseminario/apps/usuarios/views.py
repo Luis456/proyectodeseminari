3d043b279148ceca71903cd879aad70c0d93a0ba
luis <d@c.com>
2014-09-30 22:00:27 -0400

    direccionando pagina

--- proseminario/apps/usuarios/views.py
+++ proseminario/apps/usuarios/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,render_to_response
+from django.template import RequestContext
+import datetime
+
+from .models import *
 
 # Create your views here.
+def pagina_principal(request):
+	
+	return render_to_response("principal.html",RequestContext(request))
