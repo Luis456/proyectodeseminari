5493ec92dda6574838fd7625d0d4f7ef79c82c43
luis <d@c.com>
2014-10-03 21:08:05 -0400

    registro

--- proseminario/apps/usuarios/models.py
+++ proseminario/apps/usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.db import connection
 # Create your models here.
+class Registro(models.Model):
+	nick=models.CharField(max_length=150)
+	email=models.EmailField()
+	usuario=models.ForeignKey(User)
--- proseminario/apps/usuarios/views.py
+++ proseminario/apps/usuarios/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
+from django.http import HttpResponseRedirect 
 import datetime
 
 from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
+import pdb
 
 # Create your views here.
 def pagina_principal(request):
 	
 	return render_to_response("blog/principal.html",RequestContext(request))
 def registro_usuario(request):
+	if request.method=="POST":
+		form=UserCreationForm(request.POST)
+		if(form.is_valid()):
+			form.save()
+			return HttpResponseRedirect("/blog/")
+	form=UserCreationForm()
+	return render_to_response("usuario/registro.html",{"form":form},RequestContext(request))
 	
-	return render_to_response("usuario/registro.html",RequestContext(request))
